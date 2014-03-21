6941e1cdc31c00c81120d65c76d0a30ea8ecaced
kripa <itskripshere@gmail.com>
2014-03-21 18:25:29 +0530

    sabz photography

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photography::Application.config.secret_key_base = 'e9255b9dfd83180d418e90282635c0068d18698962bc3e98744e0fe7fe7ef0f9987c18448cec5cdd3198a803848d22137c8eee4fd871f33be7bd714746938455'
